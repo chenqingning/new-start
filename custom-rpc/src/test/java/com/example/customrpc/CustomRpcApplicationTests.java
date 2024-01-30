9a8fd39bbafd993cf95b7c310ff66721086d1a69
qingning <qingning>
2024-01-30 14:43:33 +0800

    akka

---
+++ custom-rpc/src/test/java/com/example/customrpc/CustomRpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.customrpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomRpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
