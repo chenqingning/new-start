ee528c64d2dae1f89878a9991ff0600d6cb59ddb
chenqingning <1241157373@qq.com>
2024-02-15 17:15:47 +0800

    添加基础java知识代码

---
+++ base-java/src/main/java/com/example/basejava/BaseJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.basejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseJavaApplication.class, args);
+    }
+
+}
---
+++ base-java/src/test/java/com/example/basejava/BaseJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
